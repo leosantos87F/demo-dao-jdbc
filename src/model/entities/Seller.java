f81e24f78dca572a2f4376884d8dc53d6f2e2ff4
Leonardo Santos <leosantos.vinte@gmail.com>
2022-12-27 18:25:58 -0300

    insert implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -22,7 +22,7 @@ public class Seller implements Serializable{
 	}
 
 
-	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+	public Seller(Integer id,String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
